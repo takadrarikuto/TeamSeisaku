6e84bb73e66c964e0dbcb35241362ee28f932c7a
河合 郁弥 <h1201819034@hiratagakuen.onmicrosoft.com>
2019-10-25 16:09:36 +0900

    10/25河合変更

--- 卒業制作作品/卒業制作作品/ObjMenu.h
+++ 卒業制作作品/卒業制作作品/ObjMenu.h
@@ -22,12 +22,20 @@ private:
 	int m_time_s;
 
 	//各残り弾数情報取得用変数(全体)
-	int sg_pb;
-	int ar_pb;
-	int sr_pb;
-	int rl_pb;
-	int rg_pb;
-	int gre_pb;
+	int sg_pb_me;
+	int ar_pb_me;
+	int sr_pb_me;
+	int rl_pb_me;
+	int rg_pb_me;
+	int gre_pb_me;
+
+	//各残り弾数情報取得用変数(リロード用)
+	int sg_pb_r;
+	int ar_pb_r;
+	int sr_pb_r;
+	int rl_pb_r;
+	int rg_pb_r;
+	int gre_pb_r;
 
 	float m_and;		//透明度処理用
 	bool m_andf;		//透明度処理用フラグ
