0428ec6e8f8c0b3d6554507c47fee68740accd8d
河合 郁弥 <h1201819034@hiratagakuen.onmicrosoft.com>
2019-11-12 12:25:09 +0900

    変更

--- 卒業制作作品/卒業制作作品/BaseStatus.h
+++ 卒業制作作品/卒業制作作品/BaseStatus.h
@@ -23,4 +23,18 @@ public:
 
 private:
 
+	//上下左右別当たり判定確認フラグ
+	bool m_UpHit_flg;    //上
+	bool m_DownHit_flg;	 //下
+	bool m_LeftHit_flg;	 //左
+	bool m_LightHit_flg; //右
+
+	//武器別ダメージ
+	int Gun_Attack; ////ハンドガン
+	int SHG_Attack;	//ショットガン
+	int AR_Attack; //アサルトライフル
+	int SR_Attack; //スナイパーライフル
+	int RL_Attack; //ロケットランチャー
+	int RG_Attack; //レールガン
+	int GRE_Attack;	//グレネード
 };
