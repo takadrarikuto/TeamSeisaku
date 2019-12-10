259e5edab2fcd876c09f51d87e31e3cb28f1a0bd
河合 郁弥 <h1201819034@hiratagakuen.onmicrosoft.com>
2019-12-10 10:08:24 +0900

    変更2

--- 卒業制作作品/卒業制作作品/ObjTitle.cpp
+++ 卒業制作作品/卒業制作作品/ObjTitle.cpp
@@ -91,8 +91,8 @@ void CObjTitle::Action()
 		{
 			m_and = 0.0f;
 			m_andf = false;
-			//Scene::SetScene(new CSceneOP());
-			Scene::SetScene(new CSceneDifficulty_Level());
+			Scene::SetScene(new CSceneOP());
+			//Scene::SetScene(new CSceneDifficulty_Level());
 		}
 	}
 
